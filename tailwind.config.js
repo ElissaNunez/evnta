078caa6fab7205b89d942156bb27e898d6651877
ElissaNunez <elissanugal_bc@hotmail.com>
2026-05-29 14:24:11 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './contexts/**/*.{js,ts,jsx,tsx}', './hooks/**/*.{js,ts,jsx,tsx}', './lib/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './sections/**/*.{js,ts,jsx,tsx}', './data/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Montserrat', 'system-ui', '-apple-system', 'sans-serif'],
